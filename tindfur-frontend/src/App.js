a5c2fa6f65687c41f74893c9646850eb7de9634a
jbh1996 <jacob.b.hopkins.96@gmail.com>
2025-04-15 12:21:24 -0700

    Testing

--- tindfur-frontend/src/App.js
+++ tindfur-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload testing this now.
+          Edit <code>src/App.js</code> and save to reload testing this right now.
         </p>
         <a
           className="App-link"
