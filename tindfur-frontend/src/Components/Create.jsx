721f823b3ba90e6ba7b56b37e8d0d8e0a90748ca
jbh1996 <jacob.b.hopkins.96@gmail.com>
2025-04-16 15:05:38 -0700

    Frontend Updates

--- tindfur-frontend/src/Components/Create.jsx
+++ tindfur-frontend/src/Components/Create.jsx
@@ -7,12 +7,21 @@ function Create({isLoggedIn, isShelter}) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("")
+
+  const createAttempt = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords don't match");
+    }
+  }
+
+
   return (
         <div className="App">
           <Header isLoggedIn={isLoggedIn} isShelter={isShelter}/>
       <main>
-      <h2>Create Account Form</h2>
-        <form>
+      <h2>Create Account</h2>
+        <form onSubmit={createAttempt}>
         <label for="profile-type">Are you a shelter or an adopter:</label>
         <select id="profile-type" name="profile-type">
         <option value="user">Adopter</option>
@@ -35,9 +44,10 @@ function Create({isLoggedIn, isShelter}) {
           <input
           type="password"
           id="confirmPassword"
-          value={password}
+          value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
           required></input>
+          <button type="submit">Submit</button>
         </form>
       </main>
       <Footer></Footer>
