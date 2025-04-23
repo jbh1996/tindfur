a1151021ee1e8f226d68b7466f5a18ab44eb4a5a
Paige Knickerbocker <103474953+knickerbockermt@users.noreply.github.com>
2025-04-23 00:09:29 -0700

    updated role select

--- tindfur-frontend/src/Components/Create.jsx
+++ tindfur-frontend/src/Components/Create.jsx
@@ -59,7 +59,9 @@ function Create({ isLoggedIn, isShelter }) {
           <h1>Create Account</h1>
           <form onSubmit={createAttempt}>
             <label for="profile-type">Are you a shelter or an adopter:</label>
-            <select id="profile-type" name="profile-type">
+            <select value={role} id="profile-type" name="profile-type"
+            onChange={(e) => setRole(e.target.value)}>
+              <option value="">Choose Account Type</option>
               <option value="user">Adopter</option>
               <option value="shelter">Shelter</option></select>
             <label>Email</label>
