df1687bec8738b61ed5af536bfc7f38be9d21d0e
Paige Knickerbocker <103474953+knickerbockermt@users.noreply.github.com>
2025-04-26 23:49:12 -0700

    start of view animals

--- tindfur-frontend/src/Components/ViewAnimals.jsx
+++ tindfur-frontend/src/Components/ViewAnimals.jsx
@@ -1,5 +1,19 @@
 import './ViewAnimals.css';
+import Footer from './Footer';
+import Header from './Header';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import userAuth from '../Hooks/UserAuth';
+import ShelterFilter from './ShelterFilter';
 
 export default function ViewAnimals() {
-    return
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <ShelterFilter />
+            </main>
+            <Footer />
+        </div>
+    )
 }
